77a69898e8c96d056c63fd1fdfc35482f647678c
PETEROH018 <mathengep63@gmail.com>
2026-05-04 17:18:32 +0300

    added text to the list element

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -6,7 +6,7 @@ const Cart = ({cart}) => {
     <div>
       <h2>Shopping Cart</h2>
       <ul>
-        {cart.map((item) => (<li key={item.id}>{item.name}</li>))}
+        {cart.map((item) => (<li key={item.id}>{item.name} is in your cart</li>))}
       </ul>
     </div>
   )
